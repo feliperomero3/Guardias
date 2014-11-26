848f1a1dcc5da9ceb10ee5a8b102a63efc45d182
Felipe Romero <admin@feliperomero.com.mx>
2014-11-25 21:38:31 -0700

    Added Lista.cs & Nodo.cs.

---
+++ Guardias/Lista.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Guardias
+{
+    /// <summary>
+    /// Lista circular dinámica (no genérica) de tipo T.
+    /// </summary>
+    /// <typeparam name="T">Cualquier primitivo o definido por el usuario</typeparam>
+    class Lista<T>
+    {
+        private Nodo<T> _inicio;
+        private Nodo<T> _fin;
+
+        public Lista()
+        {
+            _inicio = null;
+        }
+
+        public void Insertar(T x)
+        {
+            Nodo<T> p = new Nodo<T>(x);
+            if (_inicio == null)
+            {
+                _inicio = _fin = p;
+                p.Sig = _inicio;
+            }
+            else
+            {
+                p.Sig = _inicio;
+                _fin.Sig = p;
+                _fin = p;
+            }
+        }
+
+        public string Mostrar()
+        {
+            if (_inicio != null)
+            {
+                StringBuilder contenido = new StringBuilder();
+                Nodo<T> q = _inicio;
+                do
+                {
+                    contenido.AppendFormat("{0}, ", q.Entidad);
+                    q = q.Sig;
+                }
+                while (q != _inicio);
+                return contenido.ToString();
+            }
+            else
+            {
+                return "No existe lista circular.";
+            }
+        }
+    }
+}
---
+++ Guardias/Nodo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Guardias
+{
+    /// <summary>
+    /// Nodo de tipo T para usar junto con @@link Lista.cs
+    /// </summary>
+    /// <typeparam name="T">Cualquier primitivo o definido por el usuario</typeparam>
+    class Nodo<T>
+    {
+        private T _entidad;
+        private Nodo<T> _sig;
+
+        public Nodo(T x)
+        {
+            _entidad = x;
+            _sig = null;
+        }
+
+        public T Entidad
+        {
+            get { return _entidad; }
+            set { _entidad = value; }
+        }
+
+        public Nodo<T> Sig
+        {
+            get { return _sig; }
+            set { _sig = value; }
+        }
+    }
+}
